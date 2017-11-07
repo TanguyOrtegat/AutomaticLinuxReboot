b24faa4103b3e77d5c96529febee29b12e4d77a0
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2017-11-07 15:29:09 +0100

    Add files via upload

---
+++ RunServer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#config
+FXSERVERDATA=/home/FxServer/fx-server-data
+FXSERVER=/home/FxServer/fx-server
+
+#script
+
+
+echo "launching server..."
+sleep 2
+cd $FXSERVERDATA
+bash $FXSERVER/run.sh +exec server.cfg
