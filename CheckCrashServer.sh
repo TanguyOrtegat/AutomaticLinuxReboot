a27513bf0d22a7a11a7311ced621304da652d7f6
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2017-11-09 15:39:24 +0100

    Update CheckCrashServer.sh

--- CheckCrashServer.sh
+++ CheckCrashServer.sh
@@ -5,15 +5,11 @@ HOST=149.91.81.207
 PORT=30120
 
 #script
-echo "Checking server crash ..."
-sleep 2
 timeout 2 bash -c "</dev/tcp/$HOST/$PORT"; 
 
 if [ $? -ne 0 ];
 then
-	echo "Detected server crash!"
+	echo `date '+%d-%B-%Y_%H:%M:%S'` " - Detected server crash!"
 	echo "launching server again:"
 	bash /home/FxServer/reload_fxserver.sh
-else
-	echo "server up!"
 fi
