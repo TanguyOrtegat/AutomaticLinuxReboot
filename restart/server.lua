98b56221f10027ba6189cda313005e8d23e2c410
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2017-11-11 15:53:34 +0100

    Update server.lua

--- restart/server.lua
+++ restart/server.lua
@@ -1,7 +1,7 @@
 
-local text1 = "Le serveur reboot automatique dans 15 minutes ! A 15h01 / 03h01"
-local text2 = "Le serveur reboot automatique dans 10 minutes ! A 15h01 / 03h01"
-local text3 = "Le serveur reboot automatique dans 5 minutes  ! A 15h01 / 03h01 \n Pensez à sauvegarder votre position et vous déconnecté !"
+local text1 = "Le serveur reboot automatique dans 15 minutes ! A 12h01 / 00h01"
+local text2 = "Le serveur reboot automatique dans 10 minutes ! A 12h01 / 00h01"
+local text3 = "Le serveur reboot automatique dans 5 minutes  ! A 12h01 / 00h01 \n Pensez à sauvegarder votre position et vous déconnecté !"
 local test4 = "test pour avertir"
 
 RegisterServerEvent("restart:checkreboot")
@@ -9,17 +9,17 @@ RegisterServerEvent("restart:checkreboot")
 AddEventHandler('restart:checkreboot', function()
 	date_local1 = os.date('%H:%M:%S', os.time())
 	local date_local = date_local1
-	if date_local == '14:45:00' then
+	if date_local == '11:45:00' then
 		TriggerClientEvent('chatMessage', -1, "REBOOT - SERVEUR", {0, 0, 0}, text1)
-	elseif date_local == '14:50:00' then
+	elseif date_local == '11:50:00' then
 		TriggerClientEvent('chatMessage', -1, "REBOOT - SERVEUR", {0, 0, 0}, text2)
-	elseif date_local == '14:55:00' then
+	elseif date_local == '11:55:00' then
 		TriggerClientEvent('chatMessage', -1, "REBOOT - SERVEUR", {0, 0, 0}, test3)
-	elseif date_local == '02:45:00' then 
+	elseif date_local == '23:45:00' then 
 		TriggerClientEvent('chatMessage', -1, "REBOOT - SERVEUR", {0, 0, 0}, text1)
-	elseif date_local == '02:50:00' then
+	elseif date_local == '23:50:00' then
 		TriggerClientEvent('chatMessage', -1, "REBOOT - SERVEUR", {0, 0, 0}, text2)
-	elseif date_local == '02:55:00' then
+	elseif date_local == '23:55:00' then
 		TriggerClientEvent('chatMessage', -1, "REBOOT - SERVEUR", {0, 0, 0}, text3)
 	end
 end)
