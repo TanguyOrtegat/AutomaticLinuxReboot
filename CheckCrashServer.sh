d15bf53588a25fcd9afcbd409ccd8d13cbec08b5
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2017-12-28 16:32:08 +0100

    Update CheckCrashServer.sh

--- CheckCrashServer.sh
+++ CheckCrashServer.sh
@@ -5,23 +5,16 @@ HOST=5.196.127.113
 PORT=30120
 
 #script
-# timeout 2 bash -c "</dev/tcp/$HOST/$PORT"; 
-
-# if [ $? -ne 0 ];
-# then
-	# echo `date '+%d-%B-%Y_%H:%M:%S'` " - Detected server crash!"
-	# echo "launching server again:"
-	# bash /home/GTAESXServer/reload_fxserver.sh
-# else 
-	# echo `date '+%d-%B-%Y_%H:%M:%S'` " - Server on!"
-# fi
+# timeout 2 bash -c "</dev/tcp/$HOST/$PORT";
 
 (exec 3<>/dev/tcp/$HOST/$PORT) &>/dev/null
-if [ $? -ne 0 ] ; then
+
+if [ $? -ne 0 ];
+then
     echo `date '+%d-%B-%Y_%H:%M:%S'` " - Detected server crash!"
 	echo "launching server again:"
 	bash /home/GTAESXServer/reload_fxserver.sh
-    exit 1
-else
-	echo `date '+%d-%B-%Y_%H:%M:%S'` " - Server on!"
+# else 
+	# echo `date '+%d-%B-%Y_%H:%M:%S'` " - Server on!"
 fi
+
