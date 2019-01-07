ec49992fa73d807dfb5a22e1f371787d886d9c0b
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2019-01-07 23:23:05 +0100

    Update reload_fxserver.sh

--- reload_fxserver.sh
+++ reload_fxserver.sh
@@ -9,6 +9,6 @@ echo `date '+%d-%B-%Y_%H:%M:%S'` " - Emptying cache..."
 rm -R /home/fairyV3/fx-server-data/cache
 sleep 5
 echo `date '+%d-%B-%Y_%H:%M:%S'` " - Starting server..."
-screen -S "Game Server" -L ServerLog/`date '+%Y-%m-%d_%H:%M:%S'.txt` -d -m bash /root/RunServerFairyV.sh
+screen -S "Game Server" -L ServerLog/`date '+%Y-%m-%d_%H:%M:%S'.txt` -d -m bash /root/RunServer.sh
 sleep 15
 echo `date '+%d-%B-%Y_%H:%M:%S'` " - End procedure"
